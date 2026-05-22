c5e62b8a611f5a79319ae981dbfb4105d69c5a4c
Rick8011 <junior.riardo9876@email.com>
2026-05-22 10:36:00 -0300

    ADD arquivos 'include' e 'config'

--- farmacia_vav/config/conexao.php
+++ farmacia_vav/config/conexao.php
@@ -1,12 +1,14 @@
 <?php
+// Inicia a sessão para podermos usar mensagens flutuantes (Flash Messages)
+session_start();
+
 $host = "localhost";
 $dbname = "farmacia_vav";
 $usuario = "root";
-$senha = ""; // Coloque a senha do seu banco se houver
+$senha = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
-    // Define o modo de erro do PDO para exceção
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
